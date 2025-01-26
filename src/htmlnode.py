b25e6339452a270a9b2fda88922f19a95771eeda
amisark <aalfheim@gmail.com>
2025-01-26 14:11:02 -0500

    ParentNode done

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -25,7 +25,7 @@ class HTMLNode:
         retstrng = ""
         if self.children == None:
             return "None"
-        for child in children:
+        for child in self.children:
             retstrng += f"{child.tag} "
         return retstrng[:-1]
 
@@ -34,3 +34,63 @@ class HTMLNode:
     def __repr__(self):
         return(f"HTMLNode({self.tag}, {self.value}, {self.children_to_string()}, {self.props_to_html()})")
 
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("value unspecified")
+
+        props_str = ""
+        if self.props :
+            for key in self.props.keys():
+                props_str += f" {key}={self.props[key]}"
+
+        ret_str = ""
+        if not self.tag :
+            ret_str = f"{props_str[1:]} {self.value}"
+        else:
+            ret_str += f"<{self.tag}{props_str}>{self.value}</{self.tag}>"
+        return ret_str
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        
+        props_str = ""
+        if self.props :
+            for key in self.props.keys():
+                props_str += f" {key}={self.props[key]}"
+
+        ret_str = ""
+        if not self.tag :
+            raise Exception("empty tag in ParentMode")
+    
+        ret_str += f"<{self.tag}{props_str}>"
+
+        children_str = ""
+        if self.children :
+            children_str += ParentNode.get_children_str(self.children)
+
+        ret_str += f"{children_str}</{self.tag}>"
+        return ret_str
+
+    def get_children_str(children):
+        child_str = ""
+        for child in children:
+            if child.tag != None :
+                props_str = child.props_to_html()
+                child_str += f"<{child.tag} {props_str}>"
+            if child.children:
+                child_str += ParentNode.get_children_str(child.children)
+            if child.value :
+                child_str += f"{child.value}"
+            if child.tag != None :
+                child_str += f"</{child.tag}>"
+        return child_str
+        
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,7 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
-
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_html1(self):
@@ -44,5 +43,106 @@ class TestHTMLNode(unittest.TestCase):
         self.assertNotEqual(node1str, node2str)
     
 
+    def test_leaf1(self):
+        props = {
+            "href": "https://www.google.com",
+            "target": "_blank",
+        }
+        tststr = "href=https://www.google.com target=_blank"
+        node1 = LeafNode("p", "test paragraph", props)
+        node2 = LeafNode("p", "test paragraph", props)
+        leaf1str = node1.to_html()
+        leaf2str = node2.to_html()
+        print(leaf1str)
+        self.assertEqual(leaf1str, leaf2str)
+
+    def test_leaf2(self):
+        props = {
+            "href": "https://www.google.com",
+            "target": "_blank",
+        }
+        tststr = "href=https://www.google.com target=_blank"
+        node1 = LeafNode("p", "test paragraph", props)
+        leaf1str = node1.to_html()
+        leafstr_expected = "<p href=https://www.google.com target=_blank>test paragraph</p>"
+        self.assertEqual(leaf1str, leafstr_expected)
+
+
+    def test_leaf3(self):
+        node1 = LeafNode("p", "test paragraph")
+        node2 = LeafNode("p", "test paragraph")
+        leaf1str = node1.to_html()
+        leaf2str = node2.to_html()
+        print(leaf1str)
+        self.assertEqual(leaf1str, leaf2str)
+
+    def test_leaf3(self):
+        node1 = LeafNode("p", "")
+        self.assertRaises(SystemExit)
+
+    def test_leaf4(self):
+        props = {
+            "href": "https://www.google.com",
+            "target": "_blank",
+        }
+        tststr = "href=https://www.google.com target=_blank"
+        node1 = LeafNode("", "test paragraph", props)
+        leaf1str = node1.to_html()
+        leafstr_expected = "href=https://www.google.com target=_blank test paragraph"
+        self.assertEqual(leaf1str, leafstr_expected)
+
+    def test_parentmode1(self):
+        tag = "p"
+        props = None
+        children = [ LeafNode("b", "Bold text"),
+            LeafNode(None, "Normal text"),
+            LeafNode("i", "italic text"),
+            LeafNode(None, "Normal text") ]
+        node1 = ParentNode(tag, children, props)
+        print(f"pn {node1.to_html()}")
+
+    def test_parentmode2(self):
+        tag = "p"
+        props = None
+        props1 = {
+            "href": "https://www.google.com",
+            "target": "_blank",
+        }
+        children2 = [
+            HTMLNode("a", "level 2", None, props1 ),
+            LeafNode("i", "italic text")
+        ]
+        children = [ LeafNode("b", "Bold text"),
+            LeafNode(None, "Normal text"),
+            HTMLNode("a", "click here", None, props1 ),
+            LeafNode(None, "Normal text"),
+            LeafNode("a", "click here REALLY", props1 ),
+            HTMLNode("a", "click here", None, props1 ) ]
+        node1 = ParentNode(tag, children, props)
+        expected_str = "<p><b None>Bold text</b>Normal text<a href=https://www.google.com target=_blank>click here</a>Normal text<a href=https://www.google.com target=_blank>click here REALLY</a><a href=https://www.google.com target=_blank>click here</a></p>"
+        #print(f"pn2 {node1.to_html()}")
+        self.assertEqual(node1.to_html(), expected_str)
+
+    def test_parentmode3(self):
+        tag = "p"
+        props = None
+        props1 = {
+            "href": "https://www.google.com",
+            "target": "_blank",
+        }
+        children2 = [
+            HTMLNode("a", "level 2", None, props1 ),
+            LeafNode("i", "italic text")
+        ]
+        children = [ LeafNode("b", "Bold text"),
+            LeafNode(None, "Normal text"),
+            HTMLNode("a", "click here", None, props1 ),
+            LeafNode(None, "Normal text"),
+            LeafNode("a", "click here REALLY", props1 ),
+            HTMLNode("a", "click here", children2, props1 ) ]
+        node1 = ParentNode(tag, children, props)
+        expected_str="<p><b None>Bold text</b>Normal text<a href=https://www.google.com target=_blank>click here</a>Normal text<a href=https://www.google.com target=_blank>click here REALLY</a><a href=https://www.google.com target=_blank><a href=https://www.google.com target=_blank>level 2</a><i None>italic text</i>click here</a></p>"
+        #print(f"pn3 {node1.to_html()}")
+        self.assertEqual(node1.to_html(), expected_str)
 if __name__ == "__main__":
     unittest.main()
